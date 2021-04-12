1c9f85dca12fd8d05d30070f81a77a7d0195c538
Jacob <jacobliem1@gmail.com>
2021-04-12 15:49:09 -0500

    commit tree e1b3dc4ef879ec92e08e02346a49a998747e4da1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+//const employee = {
+//    name: "Sam",
+//    streetAddress: "11 Broadway"
+//}
+
+const newEmployee = {...employee};
+
+function updateEmployeeWithKeyAndValue(employee, key, value ){
+    employee[key] = value;
+
+    const newEmployee = {...employee};
+
+    return newEmployee;
+};
+
